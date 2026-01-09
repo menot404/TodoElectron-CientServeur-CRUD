b36191dfe3c16092d39234f8eec8f55dd72e1d31
Me_Not404 <m3not404@gmail.com>
2026-01-09 20:22:11 +0000

    Add environment configuration setup

--- backend/data/dbConfig.js
+++ backend/data/dbConfig.js
@@ -1,10 +1,11 @@
 const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 
-const MONGO_URI = "mongodb://localhost:27017/todoapp";
+const MONGO_URI = process.env.DATABASE_URL;
 const dbConnect = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log(`✅ Connecté à mongoDB`);
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log(`✅ Connecté à mongoDB: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error Connect: ${error.message}`);
     process.exit(1);
